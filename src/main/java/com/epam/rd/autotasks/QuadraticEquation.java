34ac4f09a8b067b3c7d7c02770c355dd7b2f6ca7
tornikekhokhiashvili <tornikexoxiashvili@gmail.com>
2024-06-02 19:26:24 +0400

    Initial commit

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -2,8 +2,6 @@ package com.epam.rd.autotasks;
 
 import java.util.Scanner;
 
-import static java.lang.Math.sqrt;
-
 public class QuadraticEquation {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
